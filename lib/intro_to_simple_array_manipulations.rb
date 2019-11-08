5cc88a3e89749f502fd2dccd28e669e5f7e3159a
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-08 22:48:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,7 +38,6 @@ def using_concat
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   @all_my_favs = @my_favorite_things.concat(more_favs)
-  @all_my_favs
 end
 
 def using_insert(array)
