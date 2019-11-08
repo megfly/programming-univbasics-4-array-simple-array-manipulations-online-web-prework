d3a60d7b791755dea78a53f2e5edcdfe829091e6
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-08 23:09:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -59,7 +59,7 @@ end
 def using_delete(instructors, no_offense_steven)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   no_offense_steven = "Steven"
-  instructors.delete(Steven)
+  instructors.delete("Steven")
 end
 
 def using_delete_at(famous_robots, integer)
